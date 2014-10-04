b69f554495f453fb48b28edf5bd2d6a09c93dc4d
陳信屹 <ossug.hychen@gmail.com>
2014-10-05 00:42:47 +0800

    pull and display 3 items blog rss.

--- frontend/app/blogposts.ls
+++ frontend/app/blogposts.ls
@@ -1,13 +1,26 @@
-{div, h2, p} = React.DOM
+{div, h2, p, a} = React.DOM
 
-BlogPost = React.createClass do 
+List = React.createClass do
   render: ->
-    h2 {}, 'title'
-    p {}, 'summary'
-    div {}, 'hiihihiii'
+    div {}, 
+      ... for let k,v of @props.items
+        div {key:k}, 
+          h2 {}, v.title
+          p {dangerouslySetInnerHTML:{__html:v.description}}, null
+          p {},
+            a {href:v.link}, \More
 
-BlogPosts = React.createClass do
+Blogroll = React.createClass do
+  mixins: [ReactFireMixin],
+  getInitialState: -> do
+    items: []
+  componentWillMount: ->
+    ref = new Firebase "https://ssx.firebaseio.com/blog/"
+    @bindAsArray ref.limit(3), \items
   render: ->
-    p {}, 'hihihihihi'
+    if @state.items.length == 0
+      div {}, 'Loading...'
+    else
+      List {items: @state.items.0}
 
-React.renderComponent BlogPosts!, document.getElementById('blogposts')
+React.renderComponent Blogroll!, document.getElementById('blogroll')
--- frontend/public/js/blogposts.js
+++ frontend/public/js/blogposts.js
@@ -1,15 +1,47 @@
-var ref$, div, h2, p, BlogPost, BlogPosts;
-ref$ = React.DOM, div = ref$.div, h2 = ref$.h2, p = ref$.p;
-BlogPost = React.createClass({
+var ref$, div, h2, p, a, List, Blogroll;
+ref$ = React.DOM, div = ref$.div, h2 = ref$.h2, p = ref$.p, a = ref$.a;
+List = React.createClass({
   render: function(){
-    h2({}, 'title');
-    p({}, 'summary');
-    return div({}, 'hiihihiii');
+    return div.apply(null, [{}].concat((function(){
+      var i$, results$ = [];
+      for (i$ in this.props.items) {
+        results$.push((fn$.call(this, i$, this.props.items[i$])));
+      }
+      return results$;
+      function fn$(k, v){
+        return div({
+          key: k
+        }, h2({}, v.title), p({
+          dangerouslySetInnerHTML: {
+            __html: v.description
+          }
+        }, null), p({}, a({
+          href: v.link
+        }, 'More')));
+      }
+    }.call(this))));
   }
 });
-BlogPosts = React.createClass({
+Blogroll = React.createClass({
+  mixins: [ReactFireMixin],
+  getInitialState: function(){
+    return {
+      items: []
+    };
+  },
+  componentWillMount: function(){
+    var ref;
+    ref = new Firebase("https://ssx.firebaseio.com/blog/");
+    return this.bindAsArray(ref.limit(3), 'items');
+  },
   render: function(){
-    return p({}, 'hihihihihi');
+    if (this.state.items.length === 0) {
+      return div({}, 'Loading...');
+    } else {
+      return List({
+        items: this.state.items[0]
+      });
+    }
   }
 });
-React.renderComponent(BlogPosts(), document.getElementById('blogposts'));
+React.renderComponent(Blogroll(), document.getElementById('blogroll'));
